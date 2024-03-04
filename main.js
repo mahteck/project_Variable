295efcc16685d9c71374244c43e7f36e8829e427
mshoaib1244 <shoaib1244@gmail.com>
2024-03-04 19:24:27 +0500

    first Commit

---
+++ main.js
@@ -0,0 +1,5 @@
+//console.log("Hello World");
+var userName = "Shoaib Munir";
+console.log(userName);
+var userFirstName = "Shoaib";
+console.log(userFirstName);
---
+++ main.ts
@@ -0,0 +1,12 @@
+// This is Shoaib MutationRecord, a Developer.
+
+// I recommend you can use this platform.
+// you can follow me at http://linkedin.com/shoaibmunir88
+
+let userName = "Shoaib Munir"
+console.log(userName);
+let userFirstName: string = "Shoaib";
+let age: number = 34;
+let isStudent: boolean = false;
+let rendomValue: any = "http://linkedin.com/shoaibmunir88";
+console.log(userFirstName);
