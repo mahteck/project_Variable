eb3f20e5870f21a9f2ba7784d89a2ecfcb769e40
mshoaib1244 <shoaib1244@gmail.com>
2024-03-04 20:00:23 +0500

    Second Commit

--- main.js
+++ main.js
@@ -1,5 +1,43 @@
-//console.log("Hello World");
-var userName = "Shoaib Munir";
-console.log(userName);
+// This is Shoaib MutationRecord, a Developer.
+// I recommend you can use this platform.
+// you can follow me at http://linkedin.com/shoaibmunir88
+// let userName = "Shoaib Munir"
+// console.log(userName);
+// Variable, Case Sensitive, Strong typing in typescript
+// data types in typescript
+//string
+// Number
+// boolean
+// any
+// variables
 var userFirstName = "Shoaib";
+var age = 34;
+var isStudent = false;
+var rendomValue = "http://linkedin.com/shoaibmunir88";
 console.log(userFirstName);
+console.log(age);
+console.log(isStudent);
+console.log(rendomValue);
+//strong type syntex
+var a = "Pakistan";
+var b = 9;
+var c = true;
+// type interface
+var e = "USA";
+var f = 10.9;
+var g = false;
+// const and let in typescript
+var currentBalance = 100; // This value can change.
+var accountNumber = "123456789";
+currentBalance = 50; // This is allowed.
+//accountNumber = "987654321"; //Error: cannot assign to 'accountNumber' because it is a constrant.
+// String Concatenation
+var firstName = "Muhammad Shoaib";
+var lastName = "Munir";
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
+// Template Literals
+var firstName1 = "Mah";
+var LastName1 = "Teck";
+var fullName1 = "".concat(firstName1, " ").concat(LastName1);
+console.log(fullName1);
--- main.ts
+++ main.ts
@@ -3,10 +3,54 @@
 // I recommend you can use this platform.
 // you can follow me at http://linkedin.com/shoaibmunir88
 
-let userName = "Shoaib Munir"
-console.log(userName);
+// let userName = "Shoaib Munir"
+// console.log(userName);
+
+// Variable, Case Sensitive, Strong typing in typescript
+// data types in typescript
+//string
+// Number
+// boolean
+// any
+
+// variables
 let userFirstName: string = "Shoaib";
 let age: number = 34;
 let isStudent: boolean = false;
 let rendomValue: any = "http://linkedin.com/shoaibmunir88";
 console.log(userFirstName);
+console.log(age);
+console.log(isStudent);
+console.log(rendomValue);
+
+
+//strong type syntex
+let a: string = "Pakistan";
+let b: number = 9;
+let c: boolean = true;
+
+// type interface
+let e = "USA"
+let f = 10.9
+let g = false
+
+// const and let in typescript
+
+let currentBalance = 100; // This value can change.
+const accountNumber = "123456789";
+
+currentBalance = 50; // This is allowed.
+//accountNumber = "987654321"; //Error: cannot assign to 'accountNumber' because it is a constrant.
+
+// String Concatenation
+
+let firstName: string = "Muhammad Shoaib";
+let lastName: string = "Munir";
+let fullName: string = firstName + ' ' + lastName;
+console.log(fullName);
+
+// Template Literals
+let firstName1: string = "Mah";
+let LastName1: string = "Teck";
+let fullName1: string = `${firstName1} ${LastName1}`;
+console.log(fullName1);
